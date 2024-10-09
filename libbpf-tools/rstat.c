8d30b21e55c1a83a51626b7d858f611db2e22698
inwardvessel <5782523+inwardvessel@users.noreply.github.com>
2024-10-08 17:23:14 -0700

    wip

--- libbpf-tools/rstat.c
+++ libbpf-tools/rstat.c
@@ -33,7 +33,9 @@ const char argp_args_doc[] =
 "        Trace and dump counts every 5 seconds\n"
 "";
 
-//static const struct argp_option argp_options[] = {};
+static const struct argp_option argp_options[] = {
+	{}
+};
 
 static struct ksyms *ksyms;
 
@@ -50,25 +52,25 @@ static void dump_histograms(const struct rstat_bpf *skel)
 	print_linear_hist(skel->bss->event_update_count, skel->rodata->max_events, 0, 1, "event");
 }
 
-//static long argp_parse_long(int key, const char *arg, struct argp_state *state);
-//static error_t argp_parse_arg(int key, char *arg, struct argp_state *state);
+static long argp_parse_long(int key, const char *arg, struct argp_state *state);
+static error_t argp_parse_arg(int key, char *arg, struct argp_state *state);
 
 int main(int argc, char *argv[])
 {
 	struct rstat_bpf *skel;
 	int i, err;
 
-	//static const struct argp argp = {
-	//	.options = argp_options,
-	//	.parser = argp_parse_arg,
-	//	.doc = argp_args_doc,
-	//};
+	static const struct argp argp = {
+		.options = argp_options,
+		.parser = argp_parse_arg,
+		.doc = argp_args_doc,
+	};
 
-	//if (argp_parse(&argp, argc, argv, 0, NULL, NULL)) {
-	//	fprintf(stderr, "failed to parse args\n");
+	if (argp_parse(&argp, argc, argv, 0, NULL, NULL)) {
+		fprintf(stderr, "failed to parse args\n");
 
-	//	return 1;
-	//}
+		return 1;
+	}
 
 	ksyms = ksyms__load();
 	if (!ksyms) {
@@ -106,42 +108,42 @@ cleanup:
 	return -err;
 }
 
-//long argp_parse_long(int key, const char *arg, struct argp_state *state)
-//{
-//	errno = 0;
-//	const long temp = strtol(arg, NULL, 10);
-//	if (errno || temp <= 0) {
-//		fprintf(stderr, "error arg:%c %s\n", (char)key, arg);
-//		argp_usage(state);
-//	}
-//
-//	return temp;
-//}
+long argp_parse_long(int key, const char *arg, struct argp_state *state)
+{
+	errno = 0;
+	const long temp = strtol(arg, NULL, 10);
+	if (errno || temp <= 0) {
+		fprintf(stderr, "error arg:%c %s\n", (char)key, arg);
+		argp_usage(state);
+	}
 
-//error_t argp_parse_arg(int key, char *arg, struct argp_state *state)
-//{
-//	static int pos_args = 0;
-//
-//	switch (key) {
-//	case ARGP_KEY_ARG:
-//		pos_args++;
-//		printf("pos arg %d\n", pos_args);
-//
-//		if (pos_args == 1) {
-//			env.interval = argp_parse_long(key, arg, state);
-//		}
-//		else if (pos_args == 2) {
-//			env.nr_intervals = argp_parse_long(key, arg, state);
-//		} else {
-//			fprintf(stderr, "Unrecognized positional argument: %s\n", arg);
-//			argp_usage(state);
-//		}
-//
-//		break;
-//	default:
-//		printf("arg unknown\n");
-//		return ARGP_ERR_UNKNOWN;
-//	}
-//
-//	return 0;
-//}
+	return temp;
+}
+
+error_t argp_parse_arg(int key, char *arg, struct argp_state *state)
+{
+	static int pos_args = 0;
+
+	switch (key) {
+	case ARGP_KEY_ARG:
+		pos_args++;
+		printf("pos arg %d\n", pos_args);
+
+		if (pos_args == 1) {
+			env.interval = argp_parse_long(key, arg, state);
+		}
+		else if (pos_args == 2) {
+			env.nr_intervals = argp_parse_long(key, arg, state);
+		} else {
+			fprintf(stderr, "Unrecognized positional argument: %s\n", arg);
+			argp_usage(state);
+		}
+
+		break;
+	default:
+		printf("arg unknown\n");
+		return ARGP_ERR_UNKNOWN;
+	}
+
+	return 0;
+}
