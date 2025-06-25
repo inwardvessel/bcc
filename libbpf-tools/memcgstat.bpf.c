f7941648bbfb52bf764b1edd84a88c3444290346
JP Kobryn <inwardvessel@gmail.com>
2025-06-25 16:09:58 -0700

    use query kfuncs and replace ring buffer with direct write
    Signed-off-by: JP Kobryn <inwardvessel@gmail.com>

--- libbpf-tools/memcgstat.bpf.c
+++ libbpf-tools/memcgstat.bpf.c
@@ -8,82 +8,28 @@
 
 char _license[] SEC("license") = "GPL";
 
-struct {
-	__uint(type, BPF_MAP_TYPE_RINGBUF);
-	__uint(max_entries, 1024); /* one kB */
-} rb SEC(".maps");
+extern void memcg_flush(struct cgroup *cgrp) __ksym;
+extern unsigned long memcg_stat_fetch(struct cgroup *cgrp, int item) __ksym;
+extern unsigned long memcg_event_fetch(struct cgroup *cgrp, int event) __ksym;
 
-struct loop_ctx {
-	struct mem_cgroup *memcg;
-};
-
-extern void mem_cgroup_flush_stats(struct mem_cgroup *memcg) __ksym;
-extern size_t memory_stat_count(void) __ksym;
-extern int memory_stat_fetch(struct mem_cgroup *memcg, int i,
-		char *name, u64 *val) __ksym;
-extern void memory_event_pgscan_fetch(struct mem_cgroup *memcg,
-		unsigned long *val) __ksym;
-extern void memory_event_pgsteal_fetch(struct mem_cgroup *memcg,
-		unsigned long *val) __ksym;
-
-static int mem_stat_submit(const char *name, u64 val)
+SEC("iter/cgroup")
+int BPF_PROG(query, struct bpf_iter_meta *meta, struct cgroup *cgrp)
 {
-	struct mem_stat_event *e;
-	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
-	if (!e)
+	struct seq_file *seq = meta->seq;
+
+	if (!cgrp)
 		return 1;
 
-	bpf_probe_read_str(&e->name, sizeof(e->name), name);
-	e->val = val;
-	bpf_ringbuf_submit(e, 0);
+	memcg_flush(cgrp);
 
-	return 0;
-}
-
-static int mem_stat_visit(u32 i, void *ctx)
-{
-	struct mem_cgroup *memcg = ((struct loop_ctx *)ctx)->memcg;
-	int err;
-	char name[64] = {};
-	u64 val = 0;
-
-	err = memory_stat_fetch(memcg, i, name, &val);
-	if (err)
-		return 1;
-
-	bpf_printk("val:%llu", val);
-	mem_stat_submit(name, val);
-
-	/* TODO handle unreclaimable b */
-
-	return 0;
-}
-
-SEC("iter.s/cgroup")
-int memcg_iter(struct bpf_iter__cgroup *ctx)
-{
-	struct cgroup *cgrp;
-	struct mem_cgroup *memcg;
-
-	cgrp = ctx->cgroup;
-	memcg = ctx->memcg;
-
-	if (!cgrp || !memcg)
-		return 1;
-
-	mem_cgroup_flush_stats(memcg);
-
-	struct loop_ctx loop_ctx = {
-		.memcg = memcg
+	int values[4] = {
+		memcg_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, NR_INACTIVE_ANON)),
+		memcg_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, NR_ACTIVE_ANON)),
+		memcg_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, NR_INACTIVE_FILE)),
+		memcg_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, NR_ACTIVE_FILE)),
 	};
-	size_t nr_mem_stats = memory_stat_count();
-	bpf_loop(nr_mem_stats, mem_stat_visit, &loop_ctx, 0);
 
-	unsigned long val = 0;
-	memory_event_pgscan_fetch(memcg, &val);
-	mem_stat_submit("pgscan", val);
-	memory_event_pgsteal_fetch(memcg, &val);
-	mem_stat_submit("pgsteal", val);
+	bpf_seq_write(seq, &values[0], sizeof(int) * 4);
 
 	return 0;
 }
--- libbpf-tools/memcgstat.c
+++ libbpf-tools/memcgstat.c
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
 // Copyright (c) 2025 Meta Platforms, Inc. and affiliates.
-
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -8,16 +7,6 @@
 #include <bpf/libbpf.h>
 #include "memcgstat.h"
 #include "memcgstat.skel.h"
-
-
-static int handle_event(void *ctx, void *data, size_t data_sz)
-{
-	struct mem_stat_event *e = data;
-
-	printf("%s %llu\n", e->name, e->val);
-
-	return 0;
-}
 
 int main()
 {
@@ -27,15 +16,6 @@ int main()
 	skel = memcgstat_bpf__open_and_load();
 	if (!skel) {
 		fprintf(stderr, "failed to open/load bpf object\n");
-		ret = 1;
-
-		goto cleanup;
-	}
-
-	struct ring_buffer *rb;
-	rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
-	if (!rb) {
-		fprintf(stderr, "failed to create ring buffer\n");
 		ret = 1;
 
 		goto cleanup;
@@ -52,7 +32,7 @@ int main()
 	opts.link_info = &linfo;
 	opts.link_info_len = sizeof(linfo);
 
-	struct bpf_link *link = bpf_program__attach_iter(skel->progs.memcg_iter, &opts);
+	struct bpf_link *link = bpf_program__attach_iter(skel->progs.query, &opts);
 	if (!link) {
 		fprintf(stderr, "link\n");
 		goto cleanup;
@@ -64,31 +44,19 @@ int main()
 		goto cleanup;
 	}
 
-	char buf[4096] = {};
-	ssize_t len;
+	int values[4] = {};
+	ssize_t len = read(iter_fd, values, sizeof(int) * 4);
+	if (len < 0) {
+		fprintf(stderr, "read\n");
+	}
+
 	int i;
-	//while ((len = read(iter_fd, buf, 0)) > 0) {
-	for (i = 0; i < 1; i++) {
-		printf("loop %d\n", i);
-
-		len = read(iter_fd, buf, sizeof(buf));
-		if (len < 0) {
-			fprintf(stderr, "read\n");
-			break;
-		}
-
-		int err = ring_buffer__poll(rb, 100);
-		if (err == -EINTR)
-			break;
-		if (err < 0)
-			fprintf(stderr, "poll\n");
-
-		lseek(iter_fd, 0, SEEK_SET);
+	for (i = 0; i < 4; i++) {
+		printf("value %d:%d\n", i, values[i]);
 	}
 
 	close(iter_fd);
 	bpf_link__destroy(link);
-	ring_buffer__free(rb);
 cleanup:
 	memcgstat_bpf__destroy(skel);
 
