ba1eeba034eecc7c861b1eb6618160d06dfba04b
JP Kobryn <inwardvessel@gmail.com>
2025-08-01 14:05:44 -0700

    use core enum funcs

--- libbpf-tools/memcgstat.bpf.c
+++ libbpf-tools/memcgstat.bpf.c
@@ -12,6 +12,20 @@ extern void memcg_flush(struct cgroup *cgrp) __ksym;
 extern unsigned long memcg_stat_fetch(struct cgroup *cgrp, int item) __ksym;
 extern unsigned long memcg_event_fetch(struct cgroup *cgrp, int event) __ksym;
 
+const volatile size_t nr_items;
+enum memcg_item items[1] SEC(".data.items");
+int results[1] SEC(".data.results");
+
+#define stat_fetch(cgrp, item) \
+	bpf_core_enum_value_exists(enum node_stat_item, item) ? \
+		 memcg_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, item)) \
+				 : -1;
+
+#define event_fetch(cgrp, item) \
+	bpf_core_enum_value_exists(enum memcg_stat_item, item) ? \
+		 memcg_event_fetch(cgrp, bpf_core_enum_value(enum memcg_stat_item, item)) \
+				 : -1;
+
 SEC("iter/cgroup")
 int BPF_PROG(query, struct bpf_iter_meta *meta, struct cgroup *cgrp)
 {
@@ -22,14 +36,136 @@ int BPF_PROG(query, struct bpf_iter_meta *meta, struct cgroup *cgrp)
 
 	memcg_flush(cgrp);
 
-	int values[4] = {
-		memcg_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, NR_INACTIVE_ANON)),
-		memcg_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, NR_ACTIVE_ANON)),
-		memcg_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, NR_INACTIVE_FILE)),
-		memcg_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, NR_ACTIVE_FILE)),
-	};
+	size_t i;
+	for (i = 0; i < nr_items; i++) {
+		switch (items[i]) {
+			case USER_NR_INACTIVE_ANON:
+				results[i] = stat_fetch(cgrp, NR_INACTIVE_ANON);
+				break;
+			case USER_NR_ACTIVE_ANON:
+				results[i] = stat_fetch(cgrp, NR_ACTIVE_ANON);
+				break;
+			case USER_NR_INACTIVE_FILE:
+				results[i] = stat_fetch(cgrp, NR_INACTIVE_FILE);
+				break;
+			case USER_NR_ACTIVE_FILE:
+				results[i] = stat_fetch(cgrp, NR_ACTIVE_FILE);
+				break;
+			case USER_NR_UNEVICTABLE:
+				results[i] = stat_fetch(cgrp, NR_UNEVICTABLE);
+				break;
+			case USER_NR_SLAB_RECLAIMABLE_B:
+				results[i] = stat_fetch(cgrp, NR_SLAB_RECLAIMABLE_B);
+				break;
+			case USER_NR_SLAB_UNRECLAIMABLE_B:
+				results[i] = stat_fetch(cgrp, NR_SLAB_UNRECLAIMABLE_B);
+				break;
+			case USER_WORKINGSET_REFAULT_ANON:
+				results[i] = stat_fetch(cgrp, WORKINGSET_REFAULT_ANON);
+				break;
+			case USER_WORKINGSET_REFAULT_FILE:
+				results[i] = stat_fetch(cgrp, WORKINGSET_REFAULT_FILE);
+				break;
+			case USER_WORKINGSET_ACTIVATE_ANON:
+				results[i] = stat_fetch(cgrp, WORKINGSET_ACTIVATE_ANON);
+				break;
+			case USER_WORKINGSET_ACTIVATE_FILE:
+				results[i] = stat_fetch(cgrp, WORKINGSET_ACTIVATE_FILE);
+				break;
+			case USER_WORKINGSET_RESTORE_ANON:
+				results[i] = stat_fetch(cgrp, WORKINGSET_RESTORE_ANON);
+				break;
+			case USER_WORKINGSET_RESTORE_FILE:
+				results[i] = stat_fetch(cgrp, WORKINGSET_RESTORE_FILE);
+				break;
+			case USER_WORKINGSET_NODERECLAIM:
+				results[i] = stat_fetch(cgrp, WORKINGSET_NODERECLAIM);
+				break;
+			case USER_NR_ANON_MAPPED:
+				results[i] = stat_fetch(cgrp, NR_ANON_MAPPED);
+				break;
+			case USER_NR_FILE_MAPPED:
+				results[i] = stat_fetch(cgrp, NR_FILE_MAPPED);
+				break;
+			case USER_NR_FILE_PAGES:
+				results[i] = stat_fetch(cgrp, NR_FILE_PAGES);
+				break;
+			case USER_NR_FILE_DIRTY:
+				results[i] = stat_fetch(cgrp, NR_FILE_DIRTY);
+				break;
+			case USER_NR_WRITEBACK:
+				results[i] = stat_fetch(cgrp, NR_WRITEBACK);
+				break;
+			case USER_NR_SHMEM:
+				results[i] = stat_fetch(cgrp, NR_SHMEM);
+				break;
+			case USER_NR_SHMEM_THPS:
+				results[i] = stat_fetch(cgrp, NR_SHMEM_THPS);
+				break;
+			case USER_NR_FILE_THPS:
+				results[i] = stat_fetch(cgrp, NR_FILE_THPS);
+				break;
+			case USER_NR_ANON_THPS:
+				results[i] = stat_fetch(cgrp, NR_ANON_THPS);
+				break;
+			case USER_NR_KERNEL_STACK_KB:
+				results[i] = stat_fetch(cgrp, NR_KERNEL_STACK_KB);
+				break;
+			case USER_NR_PAGETABLE:
+				results[i] = stat_fetch(cgrp, NR_PAGETABLE);
+				break;
+			case USER_NR_SECONDARY_PAGETABLE:
+				results[i] = stat_fetch(cgrp, NR_SECONDARY_PAGETABLE);
+				break;
+			case USER_NR_SWAPCACHE:
+				results[i] = stat_fetch(cgrp, NR_SWAPCACHE);
+				break;
+			//case USER_PGPROMOTE_SUCCESS:
+			//	results[i] = stat_fetch(cgrp, PGPROMOTE_SUCCESS);
+			//	break;
+			case USER_PGDEMOTE_KSWAPD:
+				results[i] = stat_fetch(cgrp, PGDEMOTE_KSWAPD);
+				break;
+			case USER_PGDEMOTE_DIRECT:
+				results[i] = stat_fetch(cgrp, PGDEMOTE_DIRECT);
+				break;
+			case USER_PGDEMOTE_KHUGEPAGED:
+				results[i] = stat_fetch(cgrp, PGDEMOTE_KHUGEPAGED);
+				break;
+			case USER_PGDEMOTE_PROACTIVE:
+				results[i] = stat_fetch(cgrp, PGDEMOTE_PROACTIVE);
+				break;
+			//case USER_NR_HUGETLB:
+			//	results[i] = stat_fetch(cgrp, NR_HUGETLB);
+			//	break;
+			case USER_MEMCG_SWAP:
+				results[i] = event_fetch(cgrp, MEMCG_SWAP);
+				break;
+			case USER_MEMCG_SOCK:
+				results[i] = event_fetch(cgrp, MEMCG_SOCK);
+				break;
+			case USER_MEMCG_PERCPU_B:
+				results[i] = event_fetch(cgrp, MEMCG_PERCPU_B);
+				break;
+			case USER_MEMCG_VMALLOC:
+				results[i] = event_fetch(cgrp, MEMCG_VMALLOC);
+				break;
+			case USER_MEMCG_KMEM:
+				results[i] = event_fetch(cgrp, MEMCG_KMEM);
+				break;
+			case USER_MEMCG_ZSWAP_B:
+				results[i] = event_fetch(cgrp, MEMCG_ZSWAP_B);
+				break;
+			case USER_MEMCG_ZSWAPPED:
+				results[i] = event_fetch(cgrp, MEMCG_ZSWAPPED);
+				break;
+			default:
+				results[i] = -1;
+				break;
+		}
+	}
 
-	bpf_seq_write(seq, &values[0], sizeof(int) * 4);
+	bpf_seq_write(seq, results, sizeof(int) * nr_items);
 
 	return 0;
 }
--- libbpf-tools/memcgstat.c
+++ libbpf-tools/memcgstat.c
@@ -8,21 +8,84 @@
 #include "memcgstat.h"
 #include "memcgstat.skel.h"
 
+static enum memcg_item items[] = {
+	USER_NR_INACTIVE_ANON,
+	USER_NR_ACTIVE_ANON,
+	USER_NR_INACTIVE_FILE,
+	USER_NR_ACTIVE_FILE,
+};
+
+static char *names[] = {
+	"inactive_anon",
+	"active_anon",
+	"inactive_file",
+	"active_file",
+};
+
 int main()
 {
 	int ret = 0;
 	struct memcgstat_bpf *skel;
 
-	skel = memcgstat_bpf__open_and_load();
+	skel = memcgstat_bpf__open();
 	if (!skel) {
-		fprintf(stderr, "failed to open/load bpf object\n");
+		fprintf(stderr, "failed to open bpf object\n");
 		ret = 1;
 
-		goto cleanup;
+		goto out;
+	}
+
+	skel->rodata->nr_items = sizeof(items) / sizeof(items[0]);
+	printf("nr_items:%zu\n", skel->rodata->nr_items);
+
+	size_t sz_map = sizeof(skel->data_items->items[0]) * skel->rodata->nr_items;
+	printf("desired map size:%zu\n", sz_map);
+	size_t sz_map_final;
+
+	struct bpf_map *map = skel->maps.data_items;
+	ret = bpf_map__set_value_size(map, sz_map);
+	if (ret) {
+		goto cleanup_skel;
+	}
+	skel->data_items = bpf_map__initial_value(skel->maps.data_items, &sz_map_final);
+	if (sz_map_final != sz_map) {
+		fprintf(stderr, "mismatched size\n");
+		ret = 1;
+		goto cleanup_skel;
+	}
+
+	sz_map = sizeof(skel->data_results->results[0]) * skel->rodata->nr_items;
+	printf("desired map size:%zu\n", sz_map);
+	map = skel->maps.data_results;
+	ret = bpf_map__set_value_size(map, sz_map);
+	if (ret) {
+		goto cleanup_skel;
+	}
+	skel->data_results = bpf_map__initial_value(skel->maps.data_results, &sz_map_final);
+	if (sz_map_final != sz_map) {
+		fprintf(stderr, "mismatched size\n");
+		ret = 1;
+		goto cleanup_skel;
+	}
+
+	int i;
+	for (i = 0; i < skel->rodata->nr_items; i++)
+		skel->data_items->items[i] = items[i];
+
+	ret = memcgstat_bpf__load(skel);
+	if (ret) {
+		fprintf(stderr, "failed to load bpf object\n");
+		ret = 1;
+		goto cleanup_skel;
 	}
 
 	char *path = "/sys/fs/cgroup";
 	int cgroup_fd = open(path, O_RDONLY);
+	if (cgroup_fd < 0) {
+		perror("open");
+		ret = cgroup_fd;
+		goto cleanup_skel;
+	}
 
 	DECLARE_LIBBPF_OPTS(bpf_iter_attach_opts, opts);
 	union bpf_iter_link_info linfo = {
@@ -35,30 +98,46 @@ int main()
 	struct bpf_link *link = bpf_program__attach_iter(skel->progs.query, &opts);
 	if (!link) {
 		fprintf(stderr, "link\n");
-		goto cleanup;
+		ret = 1;
+		goto cleanup_skel;
 	}
 
 	int iter_fd = bpf_iter_create(bpf_link__fd(link));
 	if (iter_fd < 0) {
 		fprintf(stderr, "create\n");
+		ret = 1;
+		goto cleanup_link;
+	}
+
+	size_t sz = sizeof(int) * skel->rodata->nr_items;
+	int *values = malloc(sz);
+	if (!values) {
+		fprintf(stderr, "no mem\n");
+		return 1;
+	}
+
+	ssize_t bytes = read(iter_fd, values, sz);
+	if (bytes < 0) {
+		perror("read");
+		ret = bytes;
+		goto cleanup;
+	}
+	if (bytes != sz) {
+		fprintf(stderr, "read: expected %zu bytes, got %zu\n", sz, bytes);
+		ret = 1;
 		goto cleanup;
 	}
 
-	int values[4] = {};
-	ssize_t len = read(iter_fd, values, sizeof(int) * 4);
-	if (len < 0) {
-		fprintf(stderr, "read\n");
+	for (i = 0; i < skel->rodata->nr_items; i++) {
+		printf("%s:%d\n", names[i], values[i]);
 	}
 
-	int i;
-	for (i = 0; i < 4; i++) {
-		printf("value %d:%d\n", i, values[i]);
-	}
-
-	close(iter_fd);
-	bpf_link__destroy(link);
 cleanup:
+	close(iter_fd);
+cleanup_link:
+	bpf_link__destroy(link);
+cleanup_skel:
 	memcgstat_bpf__destroy(skel);
-
+out:
 	return ret;
 }
