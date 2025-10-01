59ae3a073427e9fc70fc222e646f400d2db3fabd
JP Kobryn <inwardvessel@gmail.com>
2025-09-30 17:16:27 -0700

    cleanup

--- libbpf-tools/memcgstat.bpf.c
+++ libbpf-tools/memcgstat.bpf.c
@@ -9,22 +9,22 @@
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 extern void memcg_flush_stats(struct cgroup *cgrp) __ksym;
+extern unsigned long memcg_stat_fetch(struct cgroup *cgrp, enum memcg_stat_item item) __ksym;
 extern unsigned long memcg_node_stat_fetch(struct cgroup *cgrp, enum node_stat_item item) __ksym;
 extern unsigned long memcg_vm_event_fetch(struct cgroup *cgrp, enum vm_event_item item) __ksym;
-extern unsigned long memcg_stat_fetch(struct cgroup *cgrp, enum memcg_stat_item item) __ksym;
 
 const volatile size_t nr_items;
 enum memcg_item items[1] SEC(".data.items");
 long results[1] SEC(".data.results");
 
-#define node_stat_fetch_if_exists(cgrp, item) \
-	bpf_core_enum_value_exists(enum node_stat_item, item) ? \
-		 memcg_node_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, item)) \
-				 : -1;
-
 #define memcg_stat_fetch_if_exists(cgrp, item) \
 	bpf_core_enum_value_exists(enum memcg_stat_item, item) ? \
 		 memcg_node_stat_fetch(cgrp, bpf_core_enum_value(enum memcg_stat_item, item)) \
+				 : -1;
+
+#define node_stat_fetch_if_exists(cgrp, item) \
+	bpf_core_enum_value_exists(enum node_stat_item, item) ? \
+		 memcg_node_stat_fetch(cgrp, bpf_core_enum_value(enum node_stat_item, item)) \
 				 : -1;
 
 #define vm_event_fetch_if_exists(cgrp, item) \
